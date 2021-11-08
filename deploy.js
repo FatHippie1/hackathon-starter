9c8c23015110b63e5928c02d6c0822bdeddfdb5c
Terry Lucke <tlucke.lucke@gmail.com>
2021-11-07 22:51:47 -0700

    update .travis.yml with deploy script

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
     ssh
         .connect({
             // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-            host: '50.17.170.56',
+            host: '52.91.159.147',
             username: 'ubuntu',
             privateKey: 'labsuser.pem'
         })
